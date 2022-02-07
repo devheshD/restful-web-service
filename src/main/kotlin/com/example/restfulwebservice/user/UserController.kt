2b67d417552087e037ac75a8041447c7c343d798
heshAlgo <serverdeveloper@naver.com>
2022-02-07 23:47:18 +0900

    HTTP status 추가

--- src/main/kotlin/com/example/restfulwebservice/user/UserController.kt
+++ src/main/kotlin/com/example/restfulwebservice/user/UserController.kt
@@ -1,6 +1,8 @@
 package com.example.restfulwebservice.user
 
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder
 
 @RestController
 class UserController(
@@ -13,5 +15,14 @@ class UserController(
     fun retrieveUser(@PathVariable id: Int): User? = userDaoService.findOne(id)
 
     @PostMapping("/users")
-    fun createUser(@RequestBody user: User) = userDaoService.save(user)
+    fun createUser(@RequestBody user: User): ResponseEntity<User> {
+        val savedUser = userDaoService.save(user)
+
+        val location = ServletUriComponentsBuilder.fromCurrentRequest()
+            .path("/{id}")
+            .buildAndExpand(savedUser.id)
+            .toUri()
+
+        return ResponseEntity.created(location).build()
+    }
 }

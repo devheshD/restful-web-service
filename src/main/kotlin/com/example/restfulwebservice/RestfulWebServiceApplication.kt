0fa6b7ec52be86a8132536851379e2875856d11e
heshAlgo <serverdeveloper@naver.com>
2022-02-07 17:07:35 +0900

    init project

---
+++ src/main/kotlin/com/example/restfulwebservice/RestfulWebServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.restfulwebservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestfulWebServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<RestfulWebServiceApplication>(*args)
+}
